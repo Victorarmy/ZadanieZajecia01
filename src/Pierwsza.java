5c150c2593eb8a8cf6fe57ad1983c48b289992c1
Marcin Paluchowski <victorarmy@gmail.com>
2017-09-12 14:27:25 +0200

    Kolejne zmiany.

--- src/Pierwsza.java
+++ src/Pierwsza.java
@@ -2,8 +2,7 @@ public class Pierwsza {
     public static void main(String[] args) {
         System.out.println("Uczę");
         System.out.println("się");
-        System.out.println("programowania");
-        System.out.println("i");
-        System.out.println("commitowania");
+        System.out.println("commitowania.");
+        System.out.println("Coś nie działało.");
     }
 }
